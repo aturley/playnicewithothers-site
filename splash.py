b3904160e8641e67b877b8c1f1b4f414ba9b5b29
Andrew Turley <aturley@acm.org>
2011-08-07 18:12:15 -0400

    First commit.

---
+++ splash.py
@@ -0,0 +1,10 @@
+print 'Content-Type: text/html'
+print ''
+print """<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html> <head>
+<title>Play Nice With Others</title>
+</head>
+<body>
+<center><img src="/images/splash.jpg"></img></center>
+</body> </html>
+"""
